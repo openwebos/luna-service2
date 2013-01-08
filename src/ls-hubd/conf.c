openwebos/luna-service2

commit 0272fe923975c4e589d24c7e0dfb421b8729d756

Also check for WebAppMgr as application host
With the separation of LunaSysMgr and WebAppMgr into two separate
components, there is now a second executable that must be granted
special permissions. If this is not done, web applications will not be
able to call system services.

This commit is 15efd37a6bc22e4e495b54ded959768eb538714b from
webOS-ports/luna-service2.

Open-webOS-DCO-1.0-Signed-off-by: Herb Kuta <herb.kuta@palm.com>
Open-webOS-DCO-1.0-Signed-off-by: Simon Busch <morphis@gravedo.de>
Change-Id: Id0d871e007c3b0e2157ec73bab3d6402fc427f68

src/ls-hubd/conf.c
@@ -229,6 +229,12 @@ static _ConfigDOM conf_file_dom = {
                     .user_ctxt = &g_conf_sysmgr_exe_path,
                 },
                 {
+                    .key = "WebAppMgrExePath",
+                    .get_value = _ConfigKeyGetString,
+                    .user_cb = (_ConfigKeyUser*) _ConfigKeySetString,
+                    .user_ctxt = &g_conf_webappmgr_exe_path,
+                },
+                {
                     .key = "JsServiceExePath",
                     .get_value = _ConfigKeyGetString,
                     .user_cb = (_ConfigKeyUser*) _ConfigKeySetString,
@@ -279,6 +285,7 @@ char *g_conf_dynamic_service_exec_prefix = NULL; /**< prefix added to Exec in se
 int g_conf_connect_timeout_ms = 20000;           /**< timeout in ms for connect() to complete */
 char *g_conf_monitor_exe_path = NULL;           /**< path to ls-monitor */
 char *g_conf_sysmgr_exe_path = NULL;            /**< path to LunaSysMgr */
+char *g_conf_webappmgr_exe_path = NULL;         /**< path to WebAppMgr */
 char *g_conf_triton_service_exe_path = NULL;    /**< special "path" for triton services */
 char *g_conf_mojo_app_exe_path = NULL;          /**< special "path" for mojo apps */
 bool g_conf_mojo_apps_allow_all_outbound_by_default = false; /**< whether to allow mojo apps "*" outbound permissions by default */

files/conf/CMakeLists.txt
@@ -36,6 +36,7 @@ set(CONF_SECURITY_DIRECTORIES_PRV "${WEBOS_INSTALL_SYSBUS_PRVROLESDIR};${WEBOS_I
 set(CONF_SECURITY_DIRECTORIES_PUB "${WEBOS_INSTALL_SYSBUS_PUBROLESDIR};${WEBOS_INSTALL_SYSBUS_DYNPUBROLESDIR}")
 set(CONF_SECURITY_MONITOR_EXE_PATH "${WEBOS_INSTALL_BINDIR}/ls-monitor")
 set(CONF_SECURITY_SYSMGR_EXE_PATH "${WEBOS_INSTALL_BINDIR}/LunaSysMgr")
+set(CONF_SECURITY_WEBAPPMGR_EXE_PATH "${WEBOS_INSTALL_BINDIR}/WebAppMgr")
 set(CONF_SECURITY_JS_SERVICE_EXE_PATH "js")
 set(CONF_SECURITY_MOJO_APP_EXE_PATH "mojo-app")
 set(CONF_SECURITY_MOJO_APPS_ALLOW_OUTBOUND_BY_DEFAULT "true")

files/conf/ls-private.conf.in
@@ -36,6 +36,7 @@ Enabled=@CONF_SECURITY_ENABLED@
 Directories=@CONF_SECURITY_DIRECTORIES_PRV@
 MonitorExePath=@CONF_SECURITY_MONITOR_EXE_PATH@
 SysMgrExePath=@CONF_SECURITY_SYSMGR_EXE_PATH@
+WebAppMgrExePath=@CONF_SECURITY_WEBAPPMGR_EXE_PATH@
 JsServiceExePath=@CONF_SECURITY_JS_SERVICE_EXE_PATH@
 MojoAppExePath=@CONF_SECURITY_MOJO_APP_EXE_PATH@
 MojoAppsAllowAllOutboundByDefault=@CONF_SECURITY_MOJO_APPS_ALLOW_OUTBOUND_BY_DEFAULT@

files/conf/ls-public.conf.in
@@ -36,6 +36,7 @@ Enabled=@CONF_SECURITY_ENABLED@
 Directories=@CONF_SECURITY_DIRECTORIES_PUB@
 MonitorExePath=@CONF_SECURITY_MONITOR_EXE_PATH@
 SysMgrExePath=@CONF_SECURITY_SYSMGR_EXE_PATH@
+WebAppMgrExePath=@CONF_SECURITY_WEBAPPMGR_EXE_PATH@
 JsServiceExePath=@CONF_SECURITY_JS_SERVICE_EXE_PATH@
 MojoAppExePath=@CONF_SECURITY_MOJO_APP_EXE_PATH@
 MojoAppsAllowAllOutboundByDefault=@CONF_SECURITY_MOJO_APPS_ALLOW_OUTBOUND_BY_DEFAULT@

src/ls-hubd/conf.h
@@ -38,6 +38,7 @@ extern bool g_conf_log_service_status;
 extern int g_conf_connect_timeout_ms;
 extern char* g_conf_monitor_exe_path;
 extern char* g_conf_sysmgr_exe_path;
+extern char* g_conf_webappmgr_exe_path;
 extern char* g_conf_triton_service_exe_path;
 extern char* g_conf_mojo_app_exe_path;
 extern bool g_conf_mojo_apps_allow_all_outbound_by_default;

src/ls-hubd/security.c
@@ -1683,7 +1683,8 @@ _LSHubClientExePathMatches(const _LSTransportClient *client, const char *path)
 static bool
 _LSHubIsClientSysMgr(const _LSTransportClient *client)
 {
-    return _LSHubClientExePathMatches(client, g_conf_sysmgr_exe_path);
+    return _LSHubClientExePathMatches(client, g_conf_sysmgr_exe_path) ||
+           _LSHubClientExePathMatches(client, g_conf_webappmgr_exe_path);
 }
 
 /** 

commit 5037219f976232fc1322e8350cf95298463e9c43

Fix crash issue caused by commit 0272fe9
Ensure g_conf_webappmgr_exe_path is properly initialized before use.

Open-webOS-DCO-1.0-Signed-off-by: Roger Stringer <roger.stringer@palm.com>

Change-Id: Id5706fcc959aa7ce712eda7938df825546b0d1fb

.gitignore
@@ -15,7 +15,16 @@
 # limitations under the License.
 #
 # LICENSE@@@
+
+# Common Eclipse project files
+.project
+.cproject
+
+# Ignore the build artifacts.
 BUILD*
+/build
+release-*/
+debug-*/
 CMakeCache.txt
 cmake_install.cmake
 CMakeFiles

src/ls-hubd/CMakeLists.txt
@@ -28,6 +28,7 @@ add_definitions(-DG_LOG_DOMAIN="LunaServiceHub")
 
 # The following definitions avoid the need to configure any source files
 add_definitions(-DDEFAULT_SYSMGR_EXE_PATH="${WEBOS_INSTALL_BINDIR}/LunaSysMgr")
+add_definitions(-DDEFAULT_WEBAPPMGR_EXE_PATH="${WEBOS_INSTALL_BINDIR}/WebAppMgr")
 add_definitions(-DDEFAULT_MONITOR_EXE_PATH="${WEBOS_INSTALL_BINDIR}/ls-monitor")
 add_definitions(-DDEFAULT_PID_DIR="${WEBOS_INSTALL_RUNTIMEINFODIR}/ls2")
 add_definitions(-DDEFAULT_LOCAL_SOCKET_DIR="${WEBOS_INSTALL_RUNTIMEINFODIR}/ls2")

src/ls-hubd/conf.c
@@ -702,9 +702,15 @@ _ConfigSetDefaults(void)
     {
         g_conf_monitor_exe_path = g_strdup(DEFAULT_MONITOR_EXE_PATH);
     }
+
     if (!g_conf_sysmgr_exe_path)
     {
         g_conf_sysmgr_exe_path = g_strdup(DEFAULT_SYSMGR_EXE_PATH);
+    }
+
+    if (!g_conf_webappmgr_exe_path)
+    {
+        g_conf_webappmgr_exe_path = g_strdup(DEFAULT_WEBAPPMGR_EXE_PATH);
     }
 
     if (!g_conf_triton_service_exe_path)
@@ -743,6 +749,12 @@ _ConfigFreeSettings(void)
         g_free(g_conf_sysmgr_exe_path);
     }
     g_conf_sysmgr_exe_path = NULL;
+
+    if (g_conf_webappmgr_exe_path)
+    {
+        g_free(g_conf_webappmgr_exe_path);
+    }
+    g_conf_webappmgr_exe_path = NULL;
 
     if (g_conf_triton_service_exe_path)
     {
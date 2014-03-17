ed3787e603388f874583999c61cec626488246a0
Nikolay Orliuk <nikolay.orliuk@lge.com>
2014-03-17 20:37:06 +0200

    add schema for meta-data JSONs
    :Release Notes:
    Introduced service meta-data description json schema.
    
    :Detailed Notes:
    schemas for:
    - category
    - map of category name to category format
    - generic reply (to be referenced by library clients)
    
    :Testing Performed:
    jsonlint
    
    :QA Notes:
    
    :Issues Addressed:
    [BHV-1587] Design category description format
    [BHV-1588] Create JSON schema for category description
    
    Open-webOS-DCO-1.0-Signed-off-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    
    Change-Id: I4f2dfbd7428315dd035d26eadbefa6dcc89a6b6a
    Reviewed-on: https://g2g.palm.com/5003
    Reviewed-by: DCO Verification
    Reviewed-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Tested-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Reviewed-by: Anatolii Sakhnik <anatolii.sakhnik@lge.com>

---
+++ include/public/luna-service2/lunaservice-meta.h
@@ -0,0 +1,69 @@
+/****************************************************************
+ * @@@LICENSE
+ *
+ * Copyright (c) 2014 LG Electronics, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * LICENSE@@@
+ ****************************************************************/
+
+/**
+ *  @file lunaservice-meta.h
+ */
+
+#ifndef __LUNASERVICE_META_H
+#define __LUNASERVICE_META_H
+
+#include <pbnjson.h>
+
+/**
+ * @defgroup LunaServiceMeta
+ * @ingroup LunaService
+ * @brief Luna Service meta-information manipulation
+ */
+
+/**
+ * @addtogroup LunaServiceMeta
+ * @{
+ */
+
+/**
+ * Specify meta information about category
+ *
+ * Set JSON value that describes specified category. Provides validation schema
+ * for input params and replies. Gives some description for calls etc.
+ *
+ * @param sh  handle that identifies registered service on bus
+ * @param category  identifier of category this information provided for
+ * @param description  information itself (no ownership transfer)
+ *                     @ref simpleBiffService.schema "see / category in example"
+ * @param error  ouptut buffer for error description if applicable
+ * @return  false in case of error
+ */
+bool LSCategorySetDescription(
+	LSHandle *sh, const char *category,
+	jvalue_ref description,
+	LSError *error
+);
+
+/**
+ * @example simpleBiffService.schema
+ * Service description example
+ */
+
+/* TODO */
+
+/* @} END OF LunaServiceMeta */
+
+#endif
